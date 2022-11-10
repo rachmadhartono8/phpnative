ccb39cfe43aea5e3638220a48906586b9c6870d9
rachmad.hartono <rachmad.hartono@uii.ac.ic>
2022-11-10 15:54:52 +0700

    operator aritmatika in php

---
+++ todolist/operator.php
@@ -0,0 +1,18 @@
+<?php
+$a = 4;
+$b = 9;
+
+$result = $a + $b;
+var_dump($result);
+// positive to negative
+$resultnegative = -$result;
+var_dump($resultnegative);
+// negative to positive
+$resultpositive = +$result;
+var_dump($resultpositive);
+
+// modulo
+$resultmodulo = 100 % 3;
+var_dump($resultmodulo);
+
+
