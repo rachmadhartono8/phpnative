864097bf251e92b6d22511f9662dfb6ca837a0c5
rachmad.hartono <rachmad.hartono@uii.ac.ic>
2022-11-11 16:08:24 +0700

    aplikasi sederhana php

---
+++ aplikasi-todolist/Repository/TodolistRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Repository{
+    use Entity\Todolist;
+    interface Todolistrepository
+    {
+        function save(Todolist $todolist): void;
+        function remove(int $number): bolean;
+    }
+}
